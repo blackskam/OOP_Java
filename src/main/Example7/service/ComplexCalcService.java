3341981963d087e808891804ef49e0133aa556cb
unknown <MChernov@SGC01ENG02.UKMRT.local>
2023-06-30 21:40:18 +0500

    committ work_7

---
+++ src/main/Example7/service/ComplexCalcService.java
@@ -0,0 +1,7 @@
+import sem7.model.ComplexNumbers;
+
+public interface ComplexCalcService {
+    ComplexNumbers operation(ComplexNumbers n1, ComplexNumbers n2);
+
+    public void getResult(ComplexNumbers complexNum);
+}
---
+++ src/main/Example7/service/ComplexCalcServiceMultiply.java
@@ -0,0 +1,15 @@
+public class ComplexCalcServiceMultiply implements ComplexCalcService{
+
+    @Override
+    public ComplexNumbers operation(ComplexNumbers n1, ComplexNumbers n2) {
+        double resultReal = n1.getReal() * n2.getReal() - n1.getImaginary() * n2.getImaginary() ;
+        double resultImaginary = n1.getImaginary() * n2.getReal() + n1.getReal() * n2.getImaginary();
+        ComplexNumbers result = new ComplexNumbers(resultReal, resultImaginary);
+        return result;
+    }
+
+    @Override
+    public void getResult(ComplexNumbers complexNum) {
+        System.out.println("result = " + complexNum.toString());
+    }
+}
